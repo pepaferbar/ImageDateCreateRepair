8f6cb4c862a79a04e2cf3be47db50b10d1721077
Josef Ferbar <jferbar@acmark.com>
2024-05-09 12:47:25 +0200

    simplify

--- DateCreateRepair/Program.cs
+++ DateCreateRepair/Program.cs
@@ -9,94 +9,89 @@ using System.Drawing.Imaging;
 
 namespace DateCreateRepair
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+
+      string path;
+
+      path = System.AppDomain.CurrentDomain.BaseDirectory;
+
+      //path= @"c:\_temp\iCloud Photos\";
+
+      System.Threading.Thread.Sleep(500);
+
+      int count = 0;
+      int countTotal = System.IO.Directory.GetFiles(path).Count();
+
+      foreach (var filename in System.IO.Directory.GetFiles(path))
+      {
+        switch (System.IO.Path.GetExtension(filename).ToLower())
         {
-            
-            string path;
+          case ".jpg":
+          case ".jpeg":
+            count++;
 
-            path = System.AppDomain.CurrentDomain.BaseDirectory;
+            DateTime newDate = default(DateTime);
 
-            //path= @"c:\_temp\iCloud Photos\";
-
-            System.Threading.Thread.Sleep(500);
-
-            int count = 0;
-            int countTotal = System.IO.Directory.GetFiles(path).Count();
-
-            foreach (var filename in System.IO.Directory.GetFiles(path))
+            if (DateTaken(filename, ref newDate))
             {
-                switch (System.IO.Path.GetExtension(filename).ToLower())
-                {
-                    case ".jpg":
-                    case ".jpeg":
-                        count++;
+              String fName = System.IO.Path.GetFileName(filename);
+              Console.Write("{0}/{1} - {2}", count, countTotal, fName);
 
-                        DateTime newDate=default(DateTime);
+              if (newDate != System.IO.File.GetLastWriteTime(filename))
+              {
+                Console.Write(" > update");
+                System.IO.File.SetLastWriteTime(filename, newDate);
+              }
 
-                        if (DateTaken(filename, ref newDate))
-                        {
-                            String fName = System.IO.Path.GetFileName(filename);
-                            Console.Write("{0}/{1} - {2}", count, countTotal, fName);
+              Console.WriteLine("");
 
-                            if (newDate != System.IO.File.GetLastWriteTime(filename))
-                            {
-                                Console.Write(" > update");
-                                System.IO.File.SetLastWriteTime(filename, newDate);
-                            }
-
-                            Console.WriteLine("");
-
-                        }
-
-                        break;
-                }
             }
 
-            Console.WriteLine("");
-            Console.WriteLine("hotovo");
-
-            System.Threading.Thread.Sleep(2000);
+            break;
         }
+      }
 
-        static bool DateTaken(string imagepath, ref DateTime newDate)
-        {
-            try
-            {
-                //http://snipplr.com/view/25074/date-taken-exif-data-for-a-picture/
+      Console.WriteLine("");
+      Console.WriteLine("hotovo");
 
-                int DateTakenValue = 36867;
-
-                Image myImg = new Bitmap(imagepath);
-
-                string dateTakenTag = System.Text.Encoding.ASCII.GetString(myImg.GetPropertyItem(DateTakenValue).Value);
-
-                myImg.Dispose();
-
-                dateTakenTag = dateTakenTag.Substring(0, 19).Replace(" ", ":");
-
-                String[] parts = dateTakenTag.Split(':');
-
-
-                Int16 Year = Convert.ToInt16(parts[0]);
-                Int16 Month = Convert.ToInt16(parts[1]);
-                Int16 Day = Convert.ToInt16(parts[2]);
-
-                Int16 Hour = Convert.ToInt16(parts[3]);
-                Int16 Minute = Convert.ToInt16(parts[4]);
-                Int16 Second = Convert.ToInt16(parts[5]);
-
-                newDate = new DateTime(Year, Month, Day, Hour, Minute, Second);
-
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-        }
-
+      System.Threading.Thread.Sleep(2000);
     }
+
+    static bool DateTaken(string imagepath, ref DateTime newDate)
+    {
+      try
+      {
+        //http://snipplr.com/view/25074/date-taken-exif-data-for-a-picture/
+
+        int DateTakenValue = 36867;
+        string dateTakenTag = string.Empty;
+
+        using (Image myImg = new Bitmap(imagepath))
+        {
+          dateTakenTag = System.Text.Encoding.ASCII.GetString(myImg.GetPropertyItem(DateTakenValue).Value);
+          dateTakenTag = dateTakenTag.Substring(0, 19).Replace(" ", ":");
+        }
+
+        string[] parts = dateTakenTag.Split(':');
+
+        newDate = new DateTime(Convert.ToInt16(parts[0])
+                             , Convert.ToInt16(parts[1])
+                             , Convert.ToInt16(parts[2])
+                             , Convert.ToInt16(parts[3])
+                             , Convert.ToInt16(parts[4])
+                             , Convert.ToInt16(parts[5]));
+
+        return true;
+      }
+      catch (Exception)
+      {
+        return false;
+      }
+    }
+
+  }
 }
 
