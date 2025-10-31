b6247598fb8caa5a8918f254b6be104a3dff1919
ferbar <ferba@HOMEPC>
2025-10-31 19:35:18 +0100

    fic count

--- Main.cs
+++ Main.cs
@@ -8,30 +8,33 @@ namespace ConsoleApp1
 {
   public class Main
   {
+    public static int count = 0;
+    public static int countFalse = 0;
+    public static int countSkip = 0;
+    public static int countTotal = 0;
+
     public Main()
     {
       string path = System.AppDomain.CurrentDomain.BaseDirectory;
 
       //path= @"c:\_temp\iCloud Photos\";
       //path = @"d:\_temp2\___fotky_2025\_milošek\";
+      path = @"d:\_temp2\___fotky_2025\_2025\";
 
       System.Threading.Thread.Sleep(500);
 
-      int count = 0;
-      int countFalse = 0;
-      int countTotal = System.IO.Directory.GetFiles(path).Count();
+      countTotal = System.IO.Directory.GetFiles(path).Count() ;
 
       ExifData exifData = new ExifData();
 
       foreach (var filename in System.IO.Directory.GetFiles(path))
       {
+        count++;
         switch (System.IO.Path.GetExtension(filename).ToLower())
         {
           case ".jpg":
           case ".jpeg":
           case ".heic":
-            count++;
-
             DateTime newDate = default(DateTime);
             String fName = System.IO.Path.GetFileName(filename);
 
@@ -39,35 +42,39 @@ namespace ConsoleApp1
             {
               if (newDate != System.IO.File.GetLastWriteTime(filename))
               {
-                logMe(count, countTotal, fName, "update", ConsoleColor.Green);
+                logMe(fName, "update", ConsoleColor.Green);
                 System.IO.File.SetLastWriteTime(filename, newDate);
               }
               else
               {
-                logMe(count, countTotal, fName, "same date");
+                logMe(fName, "same date");
               }
             }
             else
             {
-              logMe(count, countTotal, filename, "XXX", ConsoleColor.Red);
+              logMe(filename, "XXX", ConsoleColor.Red);
               countFalse++;
             }
+            break;
+
+          default:
+            countSkip++;
             break;
         }
       }
 
       Console.WriteLine("");
 
-      Console.WriteLine($"Celkem: {countTotal}, Projito: {count}, Neaktualizované: {countFalse}");
+      Console.WriteLine($"Celkem: {countTotal}, Projito: {count}, Neaktualizované: {countFalse}, Skipnuté: {countSkip}");
 
       Console.ReadKey();
 
 
     }
 
-    private static void logMe(int count, int countTotal, string fName, string text, ConsoleColor color = ConsoleColor.Gray)
+    private static void logMe(string fName, string text, ConsoleColor color = ConsoleColor.Gray)
     {
-      Console.ForegroundColor = color;
+      Console.ForegroundColor = color;      
 
       Console.WriteLine($"{count}/{countTotal} - {fName} > {text}");
 
