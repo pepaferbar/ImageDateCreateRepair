fe3ed40f2769ac5478481dd45d19f44a51570824
ferbar <ferba@HOMEPC>
2025-10-31 18:03:37 +0100

    DateCreateRepair2

---
+++ ExifData.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MetadataExtractor;
+using MetadataExtractor.Formats.Exif;
+
+namespace ConsoleApp1
+{
+  internal class ExifData
+  {
+
+    internal bool DateTaken(string imagepath, ref DateTime newDate)
+    {
+      try
+      {
+        // 1. Načte všechny metadata z obrázku (funguje pro HEIC, JPEG, PNG...)
+        var directories = ImageMetadataReader.ReadMetadata(imagepath);
+
+        // 2. Najde EXIF adresář, kde je obvykle uložen datum pořízení.
+        //    Váš kód používal ID 36867, což je 'DateTimeOriginal'.
+        var subIfdDirectory = directories.OfType<ExifSubIfdDirectory>().FirstOrDefault();
+
+        if (subIfdDirectory != null &&
+            subIfdDirectory.TryGetDateTime(ExifDirectoryBase.TagDateTimeOriginal, out newDate))
+        {
+          // Úspěšně nalezeno a převedeno datum pořízení.
+          return true;
+        }
+
+        // 3. Záložní řešení: Někdy je datum v hlavním IFD0 adresáři
+        var ifd0Directory = directories.OfType<ExifIfd0Directory>().FirstOrDefault();
+        if (ifd0Directory != null &&
+            ifd0Directory.TryGetDateTime(ExifDirectoryBase.TagDateTimeOriginal, out newDate))
+        {
+          // Úspěšně nalezeno i zde.
+          return true;
+        }
+
+        // 4. Poslední pokus: Zkusíme tag 'DateTime' (ID 306), 
+        //    což je často datum poslední modifikace.
+        if (ifd0Directory != null &&
+            ifd0Directory.TryGetDateTime(ExifDirectoryBase.TagDateTime, out newDate))
+        {
+          // Nalezeno alespoň datum modifikace.
+          return true;
+        }
+
+        // Pokud jsme se dostali až sem, datum nebylo v metadatech nalezeno.
+        return false;
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.Message);
+        // Zachytí jakoukoliv chybu při čtení souboru nebo parsování metadat
+        return false;
+      }
+    }
+
+  }
+}
---
+++ Main.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+  public class Main
+  {
+    public Main()
+    {
+      string path = System.AppDomain.CurrentDomain.BaseDirectory;
+
+      //path= @"c:\_temp\iCloud Photos\";
+      //path = @"d:\_temp2\___fotky_2025\_milošek\";
+
+      System.Threading.Thread.Sleep(500);
+
+      int count = 0;
+      int countFalse = 0;
+      int countTotal = System.IO.Directory.GetFiles(path).Count();
+
+      ExifData exifData = new ExifData();
+
+      foreach (var filename in System.IO.Directory.GetFiles(path))
+      {
+        switch (System.IO.Path.GetExtension(filename).ToLower())
+        {
+          case ".jpg":
+          case ".jpeg":
+          case ".heic":
+            count++;
+
+            DateTime newDate = default(DateTime);
+            String fName = System.IO.Path.GetFileName(filename);
+
+            if (exifData.DateTaken(filename, ref newDate))
+            {
+              if (newDate != System.IO.File.GetLastWriteTime(filename))
+              {
+                logMe(count, countTotal, fName, "update", ConsoleColor.Green);
+                System.IO.File.SetLastWriteTime(filename, newDate);
+              }
+              else
+              {
+                logMe(count, countTotal, fName, "same date");
+              }
+            }
+            else
+            {
+              logMe(count, countTotal, filename, "XXX", ConsoleColor.Red);
+              countFalse++;
+            }
+            break;
+        }
+      }
+
+      Console.WriteLine("");
+
+      Console.WriteLine($"Celkem: {countTotal}, Projito: {count}, Neaktualizované: {countFalse}");
+
+      Console.ReadKey();
+
+
+    }
+
+    private static void logMe(int count, int countTotal, string fName, string text, ConsoleColor color = ConsoleColor.Gray)
+    {
+      Console.ForegroundColor = color;
+
+      Console.WriteLine($"{count}/{countTotal} - {fName} > {text}");
+
+      Console.ResetColor();
+    }
+  }
+
+}
