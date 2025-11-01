862a375ef162359e96a10594e5adf29118d5f3f6
ferbar <ferba@HOMEPC>
2025-11-01 18:02:20 +0100

    heic

---
+++ HeicConverter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImageMagick;
+using System;
+using System.IO;
+
+namespace DateCreateRepair2
+{
+  public class HeicConverter
+  {
+    /// <summary>
+    /// Převede obrázek ve formátu HEIC na JPG.
+    /// </summary>
+    /// <param name="heicPath">Cesta ke vstupnímu HEIC souboru.</param>
+    /// <param name="jpgPath">Cesta, kam se má uložit výsledný JPG soubor.</param>
+    /// <param name="quality">Kvalita JPG (1-100). Výchozí je 90.</param>
+    public void ConvertHeicToJpg(string heicPath, string jpgPath, int quality = 90)
+    {
+      // Použití 'using' je klíčové pro správné uvolnění zdrojů
+      using (MagickImage image = new MagickImage(heicPath))
+      {
+        // Nastavení kvality pro JPG
+        image.Quality = (uint)quality; 
+
+        // Nastavení výstupního formátu
+        image.Format = MagickFormat.Jpg;
+
+        // Uložení souboru
+        image.Write(jpgPath);
+      }
+    }
+  }
+}
--- Main.cs
+++ Main.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DateCreateRepair2;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ namespace ConsoleApp1
   {
     public static int count = 0;
     public static int countFalse = 0;
+    public static int countTrue = 0;
     public static int countSkip = 0;
     public static int countTotal = 0;
 
@@ -19,14 +21,45 @@ namespace ConsoleApp1
 
       //path= @"c:\_temp\iCloud Photos\";
       //path = @"d:\_temp2\___fotky_2025\_milošek\";
-      path = @"d:\_temp2\___fotky_2025\_2025\";
+      //path = @"d:\_temp2\___fotky_2025\_2025\";
 
       System.Threading.Thread.Sleep(500);
 
-      countTotal = System.IO.Directory.GetFiles(path).Count() ;
+      countTotal = System.IO.Directory.GetFiles(path).Count();
 
       ExifData exifData = new ExifData();
+      HeicConverter heicConverter = new HeicConverter();
 
+      //heic conver
+      foreach (var filename in System.IO.Directory.GetFiles(path))
+      {
+        switch (System.IO.Path.GetExtension(filename).ToLower())
+        {
+          case ".heic":
+            try
+            {
+              string fName = System.IO.Path.GetFileName(filename);
+              string jpgCesta = Path.ChangeExtension(filename, ".jpg");
+
+              Console.WriteLine($"Zahajuji převod... {fName}");
+
+              heicConverter.ConvertHeicToJpg(filename, jpgCesta, 95);
+              Console.WriteLine($"Hotovo! Soubor uložen do: {jpgCesta}");
+
+              string bakCesta = filename + ".bak";
+              File.Move(filename, bakCesta);
+              Console.WriteLine($"Hotovo! Soubor přejmenován na: {bakCesta}");
+
+            }
+            catch (Exception ex)
+            {
+              Console.WriteLine($"Chyba při převodu: {ex.Message}");
+            }            
+            break;
+        }
+      }
+
+      //DateTaken
       foreach (var filename in System.IO.Directory.GetFiles(path))
       {
         count++;
@@ -34,14 +67,15 @@ namespace ConsoleApp1
         {
           case ".jpg":
           case ".jpeg":
-          case ".heic":
+            //case ".heic":
             DateTime newDate = default(DateTime);
-            String fName = System.IO.Path.GetFileName(filename);
+            string fName = System.IO.Path.GetFileName(filename);
 
             if (exifData.DateTaken(filename, ref newDate))
             {
               if (newDate != System.IO.File.GetLastWriteTime(filename))
               {
+                countTrue++;
                 logMe(fName, "update", ConsoleColor.Green);
                 System.IO.File.SetLastWriteTime(filename, newDate);
               }
@@ -52,8 +86,8 @@ namespace ConsoleApp1
             }
             else
             {
-              logMe(filename, "XXX", ConsoleColor.Red);
               countFalse++;
+              logMe(filename, "not date", ConsoleColor.DarkRed);
             }
             break;
 
@@ -65,16 +99,14 @@ namespace ConsoleApp1
 
       Console.WriteLine("");
 
-      Console.WriteLine($"Celkem: {countTotal}, Projito: {count}, Neaktualizované: {countFalse}, Skipnuté: {countSkip}");
+      Console.WriteLine($"Celkem: {countTotal}, Projito: {count}, Aktualizované: {countTrue}, Neaktualizované: {countFalse}, Skipnuté: {countSkip}");
 
       Console.ReadKey();
-
-
     }
 
     private static void logMe(string fName, string text, ConsoleColor color = ConsoleColor.Gray)
     {
-      Console.ForegroundColor = color;      
+      Console.ForegroundColor = color;
 
       Console.WriteLine($"{count}/{countTotal} - {fName} > {text}");
 
